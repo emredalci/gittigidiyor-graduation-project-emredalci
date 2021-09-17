0eaf4bdfc6110eaeae4560063401cab7d4f00090
Emre <emredalc@gmail.com>
2021-09-17 23:34:52 +0300

    Add mapping CustomerUpdateDTO to Customer

--- customer-service/src/main/java/dev/patika/customerservice/mapper/CustomerMapper.java
+++ customer-service/src/main/java/dev/patika/customerservice/mapper/CustomerMapper.java
@@ -2,6 +2,7 @@ package dev.patika.customerservice.mapper;
 
 import dev.patika.customerservice.dto.CustomerDTO;
 import dev.patika.customerservice.dto.CustomerResponseDTO;
+import dev.patika.customerservice.dto.CustomerUpdateDTO;
 import dev.patika.customerservice.model.Customer;
 import org.mapstruct.Mapper;
 
@@ -9,5 +10,6 @@ import org.mapstruct.Mapper;
 public interface CustomerMapper {
 
     Customer mapFromCustomerDTOtoCustomer(CustomerDTO customerDTO);
+    Customer mapFromCustomerUpdateDTOtoCustomer(CustomerUpdateDTO customerUpdateDTO);
     CustomerResponseDTO mapFromCustomertoCustomerResponseDTO(Customer customer);
 }
