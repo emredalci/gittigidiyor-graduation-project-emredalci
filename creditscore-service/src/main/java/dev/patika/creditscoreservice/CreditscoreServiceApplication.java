8567018176b7f3495606db27a2b0bd8e94c5abf7
Emre <emredalc@gmail.com>
2021-09-18 00:16:33 +0300

    Add annotation to enable swagger to CreditscoreServiceApplication.java

--- creditscore-service/src/main/java/dev/patika/creditscoreservice/CreditscoreServiceApplication.java
+++ creditscore-service/src/main/java/dev/patika/creditscoreservice/CreditscoreServiceApplication.java
@@ -2,8 +2,10 @@ package dev.patika.creditscoreservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CreditscoreServiceApplication {
 
     public static void main(String[] args) {
