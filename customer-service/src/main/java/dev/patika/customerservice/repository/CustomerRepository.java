b54bc21f34e4afd1d8cc37609c836ffe4036f583
Emre <emredalc@gmail.com>
2021-09-17 22:45:37 +0300

    Create CustomerRepository.java

---
+++ customer-service/src/main/java/dev/patika/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package dev.patika.customerservice.repository;
+
+import dev.patika.customerservice.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
