d5eea9a9a9401d4899cccd61b314049cd09b9c56
Emre <emredalc@gmail.com>
2021-09-17 23:10:59 +0300

    Create CustomerResponseDTO.java

---
+++ customer-service/src/main/java/dev/patika/customerservice/dto/CustomerResponseDTO.java
@@ -0,0 +1,35 @@
+package dev.patika.customerservice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerResponseDTO {
+
+    @NotNull
+    private long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String nationalId;
+
+    @NotNull
+    private double income;
+
+    @NotBlank
+    private String phoneNumber;
+
+}
