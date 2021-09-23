e5fd35e3ad5347e090b7d5185852dab8367eaf69
Emre <emredalc@gmail.com>
2021-09-23 20:33:48 +0300

    Add new error mesaage

--- customer-service/src/main/java/dev/patika/customerservice/util/ErrorMessage.java
+++ customer-service/src/main/java/dev/patika/customerservice/util/ErrorMessage.java
@@ -2,4 +2,5 @@ package dev.patika.customerservice.util;
 
 public class ErrorMessage {
     public static final String CUSTOMER_IS_ALREADY_EXIST = "Customer is already exist";
+    public static final String CUSTOMER_IS_NOT_FOUND = "Customer is not found";
 }
