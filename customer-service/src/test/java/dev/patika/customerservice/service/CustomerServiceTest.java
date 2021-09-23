af6e6e0d06dca1434788bbed4b2760356c9b83be
Emre <emredalc@gmail.com>
2021-09-23 20:22:57 +0300

    Add saveCustomer method test

---
+++ customer-service/src/test/java/dev/patika/customerservice/service/CustomerServiceTest.java
@@ -0,0 +1,55 @@
+package dev.patika.customerservice.service;
+
+import dev.patika.customerservice.dto.CustomerDTO;
+import dev.patika.customerservice.dto.CustomerResponseDTO;
+import dev.patika.customerservice.mapper.CustomerMapper;
+import dev.patika.customerservice.model.Customer;
+import dev.patika.customerservice.repository.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CustomerServiceTest {
+
+    @Mock
+    CustomerRepository customerRepository;
+
+    @Mock
+    CustomerMapper customerMapper;
+
+    @InjectMocks
+    CustomerService customerService;
+
+
+
+    @Test
+    void saveCustomer() {
+        Customer customer = new Customer();
+        CustomerResponseDTO expected = new CustomerResponseDTO();
+        when(customerRepository.existsByNationalId(null)).thenReturn(Boolean.FALSE);
+        when(customerMapper.mapFromCustomerDTOtoCustomer(any())).thenReturn(customer);
+        when(customerRepository.save(customer)).thenReturn(customer);
+        when(customerMapper.mapFromCustomertoCustomerResponseDTO(customer)).thenReturn(expected);
+        //when
+        CustomerDTO customerDTO = new CustomerDTO();
+        CustomerResponseDTO actual =customerService.saveCustomer(customerDTO);
+        //then
+        assertAll(
+                ()-> assertNotNull(actual),
+                () ->assertNotNull(expected),
+                ()-> assertEquals(expected.getClass(),actual.getClass()),
+                ()-> assertEquals(expected.getFirstName(),actual.getFirstName()),
+                ()-> assertEquals(expected.getLastName(),actual.getLastName()),
+                ()-> assertEquals(expected.getNationalId(),actual.getNationalId()),
+                ()-> assertEquals(expected.getPhoneNumber(),actual.getPhoneNumber()),
+                ()-> assertEquals(expected.getIncome(),actual.getIncome())
+        );
+    }
+}
