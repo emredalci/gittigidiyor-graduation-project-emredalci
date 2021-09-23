db17917013878399110917528e2c6e7dadca602b
Emre <emredalc@gmail.com>
2021-09-23 20:36:43 +0300

    Add updateCustomer test

--- customer-service/src/test/java/dev/patika/customerservice/service/CustomerServiceTest.java
+++ customer-service/src/test/java/dev/patika/customerservice/service/CustomerServiceTest.java
@@ -2,6 +2,7 @@ package dev.patika.customerservice.service;
 
 import dev.patika.customerservice.dto.CustomerDTO;
 import dev.patika.customerservice.dto.CustomerResponseDTO;
+import dev.patika.customerservice.dto.CustomerUpdateDTO;
 import dev.patika.customerservice.mapper.CustomerMapper;
 import dev.patika.customerservice.model.Customer;
 import dev.patika.customerservice.repository.CustomerRepository;
@@ -11,8 +12,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -52,4 +56,25 @@ class CustomerServiceTest {
                 ()-> assertEquals(expected.getIncome(),actual.getIncome())
         );
     }
+
+    @Test
+    void updateCustomer() {
+        //given
+        Customer customer = new Customer();
+        CustomerResponseDTO expected = new CustomerResponseDTO();
+        when(customerRepository.findByNationalId(anyString())).thenReturn(Optional.of(customer));
+        when(customerRepository.save(customer)).thenReturn(customer);
+        when(customerMapper.mapFromCustomertoCustomerResponseDTO(customer)).thenReturn(expected);
+        //when
+        CustomerUpdateDTO customerUpdateDTO = new CustomerUpdateDTO();
+        String nationalId = "13758028554";
+        CustomerResponseDTO actual =customerService.updateCustomer(customerUpdateDTO,nationalId);
+        //then
+        assertAll(
+                ()-> assertNotNull(actual),
+                ()-> assertNotNull(expected),
+                ()-> assertEquals(expected.getId(),actual.getId())
+        );
+    }
+
 }
