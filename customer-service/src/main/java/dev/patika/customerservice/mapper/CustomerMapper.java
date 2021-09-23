9508bbafc8451da87a51e8337f03ef14ce422c46
Emre <emredalc@gmail.com>
2021-09-23 20:00:52 +0300

    Update CustomerMapper -> Add instance for testing -> Add @MapiingTarget for bussiness layer

--- customer-service/src/main/java/dev/patika/customerservice/mapper/CustomerMapper.java
+++ customer-service/src/main/java/dev/patika/customerservice/mapper/CustomerMapper.java
@@ -5,11 +5,15 @@ import dev.patika.customerservice.dto.CustomerResponseDTO;
 import dev.patika.customerservice.dto.CustomerUpdateDTO;
 import dev.patika.customerservice.model.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CustomerMapper {
 
+    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+
     Customer mapFromCustomerDTOtoCustomer(CustomerDTO customerDTO);
-    Customer mapFromCustomerUpdateDTOtoCustomer(CustomerUpdateDTO customerUpdateDTO);
+    void mapFromCustomerUpdateDTOtoCustomer(CustomerUpdateDTO customerUpdateDTO, @MappingTarget Customer customer);
     CustomerResponseDTO mapFromCustomertoCustomerResponseDTO(Customer customer);
 }
