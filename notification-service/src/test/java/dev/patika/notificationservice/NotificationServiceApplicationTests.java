1b8a864b944a943df635a49f3e762a3069467419
Emre <emredalc@gmail.com>
2021-09-23 22:25:25 +0300

    Create notification-service

---
+++ notification-service/src/test/java/dev/patika/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
