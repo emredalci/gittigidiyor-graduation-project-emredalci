e08c12b386929894c878a1195471f642a90f20e0
Emre <emredalc@gmail.com>
2021-09-23 19:51:48 +0300

    Update pattern

--- customer-service/src/main/java/dev/patika/customerservice/dto/CustomerDTO.java
+++ customer-service/src/main/java/dev/patika/customerservice/dto/CustomerDTO.java
@@ -1,9 +1,11 @@
 package dev.patika.customerservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -19,27 +21,28 @@ public class CustomerDTO {
 
     @ApiModelProperty(example = "Emre")
     @NotBlank(message = "First name is mandatory")
-    @Pattern(regexp = "/^([^0-9]*)$/")
+    @Pattern(regexp = "^[\\p{L}]+([(\\s)$]?+[\\p{L}]+)+",message = "Invalid first name")
     private String firstName;
 
     @ApiModelProperty(example = "Dalcı")
     @NotBlank(message = "Last name is mandatory")
-    @Pattern(regexp = "/^([^0-9]*)$/")
+    @Pattern(regexp = "^[\\p{L}]+([(\\s)$]?+[\\p{L}]+)+",message = "Invalid last name")
     private String lastName;
 
     @ApiModelProperty(example = "11111111111")
     @NotBlank(message = "National id is mandatory")
-    @Pattern(regexp = "^[1-9]{1}[0-9]{9}[02468]{1}$")
+    @Pattern(regexp = "^[1-9]{1}[0-9]{9}[02468]{1}$",message = "Invalid national id")
     private String nationalId;
 
     @ApiModelProperty(example = "1000")
     @NotNull(message = "Income is mandatory")
-    @Pattern(regexp = "/^[0-9]*$/")
+    @Range(max = 100000,min = 0,message = "Invalid income")
+    @JsonProperty(required = true)
     private double income;
 
     @ApiModelProperty(example = "05391111111")
     @NotBlank(message = "Phone number is mandatory")
-    @Pattern(regexp = "/^(05)([0-9]{2})\\s?([0-9]{3})\\s?([0-9]{2})\\s?([0-9]{2})$/")
+    @Pattern(regexp = "^(05)([0-9]{2})\\s?([0-9]{3})\\s?([0-9]{2})\\s?([0-9]{2})$",message = "Invalid phone number")
     private String phoneNumber;
 
 
