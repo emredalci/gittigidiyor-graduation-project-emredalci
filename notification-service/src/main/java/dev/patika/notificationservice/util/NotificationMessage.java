7bf62dbd903174592b96ae382131f026cc8ead47
Emre <emredalc@gmail.com>
2021-09-24 00:08:20 +0300

    Create NotificationMessage

---
+++ notification-service/src/main/java/dev/patika/notificationservice/util/NotificationMessage.java
@@ -0,0 +1,7 @@
+package dev.patika.notificationservice.util;
+
+public class NotificationMessage {
+
+    public static final String  SEND_SMS_NOTIFICATION_BODY ="Your application is accepted";
+    public static final String NOTIFICATION_IS_SUCCESS ="Notification process is success";
+}
