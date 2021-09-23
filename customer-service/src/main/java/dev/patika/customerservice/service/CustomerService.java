79172a9c3b733d0253057bbae042699eb486b06f
Emre <emredalc@gmail.com>
2021-09-24 00:32:17 +0300

    Add appForLoan method

--- customer-service/src/main/java/dev/patika/customerservice/service/CustomerService.java
+++ customer-service/src/main/java/dev/patika/customerservice/service/CustomerService.java
@@ -6,14 +6,20 @@ import dev.patika.customerservice.dto.CustomerUpdateDTO;
 import dev.patika.customerservice.exception.CustomerIsAlreadyExistException;
 import dev.patika.customerservice.exception.NotFoundCustomerException;
 import dev.patika.customerservice.mapper.CustomerMapper;
+import dev.patika.customerservice.model.CreditResult;
 import dev.patika.customerservice.model.Customer;
+import dev.patika.customerservice.repository.CreditResultRepository;
+import dev.patika.customerservice.repository.CreditScoreRepository;
 import dev.patika.customerservice.repository.CustomerRepository;
+import dev.patika.customerservice.util.CalculateCreditResult;
 import dev.patika.customerservice.util.ErrorMessage;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
@@ -23,6 +29,9 @@ import java.util.Optional;
 public class CustomerService implements BaseService<CustomerService>{
     private final CustomerRepository customerRepository;
     private final CustomerMapper customerMapper;
+    private final RestTemplate restTemplate;
+    private final CreditScoreRepository creditScoreRepository;
+    private final CreditResultRepository creditResultRepository;
 
     Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
@@ -69,6 +78,26 @@ public class CustomerService implements BaseService<CustomerService>{
         customerRepository.deleteByNationalId(nationalId);
         logger.info("Customer Service delete customer process is done successfully");
         return customerResponseDTO;
+    }
+
+    public String applyForLoan(String nationalId){
+        logger.info("Customer Service credit card application process is started");
+        Optional<Customer> customer = customerRepository.findByNationalId(nationalId);
+        if (!customer.isPresent()){
+            logger.error("Customer service applying for loan process error : "+ErrorMessage.CUSTOMER_IS_NOT_FOUND);
+            throw new NotFoundCustomerException(ErrorMessage.CUSTOMER_IS_NOT_FOUND);
+        }
+        char lastNumber = nationalId.charAt(10);
+        double score = creditScoreRepository.findCreditScore(lastNumber);
+        CreditResult creditResult= CalculateCreditResult.calculate(score,customer.get());
+        creditResultRepository.save(creditResult);
+        logger.info("Notification service is calling");
+        ResponseEntity<String> smsNotification =
+                restTemplate.getForEntity("http://NOTIFICATION-SERVICE/notification/"+nationalId+"/"+customer.get().getPhoneNumber(),String.class);
+        logger.info(smsNotification.getBody());
+        logger.info("Customer Service credit card application process is done successfully");
+        return creditResult.toString();
+        
     }
 
 
