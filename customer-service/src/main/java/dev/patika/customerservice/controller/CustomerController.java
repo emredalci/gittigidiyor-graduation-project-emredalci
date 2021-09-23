204dc52141d351b31526b1e7dc7d9fd8bd8c7917
Emre <emredalc@gmail.com>
2021-09-23 20:38:21 +0300

    Add put mapping updateCustomer

--- customer-service/src/main/java/dev/patika/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/dev/patika/customerservice/controller/CustomerController.java
@@ -2,13 +2,11 @@ package dev.patika.customerservice.controller;
 
 import dev.patika.customerservice.dto.CustomerDTO;
 import dev.patika.customerservice.dto.CustomerResponseDTO;
+import dev.patika.customerservice.dto.CustomerUpdateDTO;
 import dev.patika.customerservice.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,5 +19,10 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<CustomerResponseDTO> saveCustomer(@RequestBody @Valid CustomerDTO customerDTO){
         return ResponseEntity.ok(customerService.saveCustomer(customerDTO));
+    }
+
+    @PutMapping("/{nationalId}")
+    public ResponseEntity<CustomerResponseDTO> updateCustomer(@RequestBody @Valid CustomerUpdateDTO customerUpdateDTO, @PathVariable String nationalId){
+        return ResponseEntity.ok(customerService.updateCustomer(customerUpdateDTO,nationalId));
     }
 }
