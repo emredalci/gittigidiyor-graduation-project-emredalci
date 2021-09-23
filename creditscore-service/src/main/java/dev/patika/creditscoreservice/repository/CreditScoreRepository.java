b85fb94091e438e8cc64be4483067c10249a174c
Emre <emredalc@gmail.com>
2021-09-23 19:38:52 +0300

    Delete credit score service

--- creditscore-service/src/main/java/dev/patika/creditscoreservice/repository/CreditScoreRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.patika.creditscoreservice.repository;
-
-import dev.patika.creditscoreservice.model.CreditScore;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CreditScoreRepository extends JpaRepository<CreditScore,Long> {
-}
