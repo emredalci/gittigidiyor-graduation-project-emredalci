8195c3ee667bfd1dd1fc9f30d81aa821a5adb031
Emre <emredalc@gmail.com>
2021-09-24 00:44:29 +0300

    Create CreditResultController

---
+++ customer-service/src/main/java/dev/patika/customerservice/controller/CreditResultController.java
@@ -0,0 +1,26 @@
+package dev.patika.customerservice.controller;
+
+
+import dev.patika.customerservice.dto.CreditResultResponseDTO;
+import dev.patika.customerservice.service.CreditResultService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/result")
+public class CreditResultController {
+
+    private final CreditResultService creditResultService;
+
+    @GetMapping("/{nationalId}")
+    public ResponseEntity<List<CreditResultResponseDTO>> findCreditResult(@PathVariable String nationalId){
+        return ResponseEntity.ok(creditResultService.findByNationalId(nationalId));
+    }
+}
