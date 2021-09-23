55c428b6a20705e2f07c5a690424607aae2e5f46
Emre <emredalc@gmail.com>
2021-09-23 19:58:18 +0300

    Create CreditResultDTO.java

---
+++ customer-service/src/main/java/dev/patika/customerservice/dto/CreditResultDTO.java
@@ -0,0 +1,24 @@
+package dev.patika.customerservice.dto;
+
+
+import dev.patika.customerservice.model.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditResultDTO {
+    @NotNull
+    private Status status;
+
+    @NotNull
+    private double creditLimit;
+
+    @NotNull
+    private String customerNationalId;
+
+}
