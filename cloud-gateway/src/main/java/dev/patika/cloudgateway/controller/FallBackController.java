c1168d2f5cf4b8cec43c70288a7446a58eed0437
Emre <emredalc@gmail.com>
2021-09-23 23:19:02 +0300

    Create FallBackController

---
+++ cloud-gateway/src/main/java/dev/patika/cloudgateway/controller/FallBackController.java
@@ -0,0 +1,18 @@
+package dev.patika.cloudgateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackController {
+
+    @GetMapping("/customerServiceFallBack")
+    public String customerServiceFallBackMethod(){
+        return "Customer service is taking longer than expected. Please try again ";
+    }
+
+    @GetMapping("/notificationServiceServiceFallback")
+    public String notificationServiceFallBackMethod(){
+        return "Notification service is taking longer than expected. Please try again";
+    }
+}
