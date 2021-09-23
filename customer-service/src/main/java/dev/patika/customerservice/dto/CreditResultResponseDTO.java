382cb576611ca1bd5caabed93feff3bf1f0df962
Emre <emredalc@gmail.com>
2021-09-23 20:03:20 +0300

    Rename CreditResultResponseDTO.java

--- customer-service/src/main/java/dev/patika/customerservice/dto/CreditResultResponseDTO.java
+++ customer-service/src/main/java/dev/patika/customerservice/dto/CreditResultResponseDTO.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotNull;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreditResultDTO {
+public class CreditResultResponseDTO {
     @NotNull
     private Status status;
 
