7bfc324469d15aabc44042034c16898c5ecaa814
Emre <emredalc@gmail.com>
2021-09-23 20:29:00 +0300

    Add saveCustomer method test

---
+++ customer-service/src/test/java/dev/patika/customerservice/controller/CustomerControllerTest.java
@@ -0,0 +1,48 @@
+package dev.patika.customerservice.controller;
+
+import dev.patika.customerservice.dto.CustomerDTO;
+import dev.patika.customerservice.dto.CustomerResponseDTO;
+import dev.patika.customerservice.service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CustomerControllerTest {
+
+    @Mock
+    CustomerService customerService;
+
+    @InjectMocks
+    CustomerController customerController;
+
+    @Test
+    void saveCustomer() {
+        //when
+        CustomerResponseDTO expected = new CustomerResponseDTO();
+        when(customerService.saveCustomer(any())).thenReturn(expected);
+        //given
+        CustomerDTO customerDTO = new CustomerDTO();
+        ResponseEntity<CustomerResponseDTO> actual = customerController.saveCustomer(customerDTO);
+        //then
+        assertAll(
+                ()-> assertNotNull(actual),
+                ()-> assertNotNull(expected),
+                ()-> assertEquals(expected.getClass(),actual.getBody().getClass()),
+                ()-> assertEquals(expected.getFirstName(),actual.getBody().getFirstName()),
+                ()-> assertEquals(expected.getLastName(),actual.getBody().getLastName()),
+                ()-> assertEquals(expected.getNationalId(),actual.getBody().getNationalId()),
+                ()-> assertEquals(expected.getPhoneNumber(),actual.getBody().getPhoneNumber()),
+                ()-> assertEquals(expected.getIncome(),actual.getBody().getIncome())
+
+        );
+
+    }
+}
