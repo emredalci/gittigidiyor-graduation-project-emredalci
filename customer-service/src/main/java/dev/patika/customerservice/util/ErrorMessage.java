08af49c4832cbb6cb4d97c99ed9e3b6d244d9446
Emre <emredalc@gmail.com>
2021-09-24 00:30:08 +0300

    Add some error messages

--- customer-service/src/main/java/dev/patika/customerservice/util/ErrorMessage.java
+++ customer-service/src/main/java/dev/patika/customerservice/util/ErrorMessage.java
@@ -3,4 +3,6 @@ package dev.patika.customerservice.util;
 public class ErrorMessage {
     public static final String CUSTOMER_IS_ALREADY_EXIST = "Customer is already exist";
     public static final String CUSTOMER_IS_NOT_FOUND = "Customer is not found";
+    public static final String CREDIT_RESULT_NOT_CREATED = "Credit Result can not be created";
+    public static final String JSON_PARSE_ERROR = "Json parse error";
 }
