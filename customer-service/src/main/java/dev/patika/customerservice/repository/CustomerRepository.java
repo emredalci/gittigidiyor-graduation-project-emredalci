ba16b575b97e2d0a2f4d5f4eea1fbc272ba0887f
Emre <emredalc@gmail.com>
2021-09-23 20:05:22 +0300

    Update CustomerRepository -> Add some useful method signatures

--- customer-service/src/main/java/dev/patika/customerservice/repository/CustomerRepository.java
+++ customer-service/src/main/java/dev/patika/customerservice/repository/CustomerRepository.java
@@ -4,6 +4,14 @@ import dev.patika.customerservice.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+    boolean existsByNationalId(String nationalId);
+
+    Optional<Customer> findByNationalId(String nationalId);
+
+    void deleteByNationalId(String nationalId);
 }
