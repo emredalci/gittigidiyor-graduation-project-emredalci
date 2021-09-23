bd21d98b36a5e8f915474968932ff441bfc3a9ee
Emre <emredalc@gmail.com>
2021-09-23 22:44:35 +0300

    Create cloud-gateway

---
+++ cloud-gateway/src/test/java/dev/patika/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
