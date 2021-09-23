0df11fdbcd4e319562f95df95e18ff998475917b
Emre <emredalc@gmail.com>
2021-09-23 23:27:54 +0300

    Create AbstractBaseEntity

---
+++ notification-service/src/main/java/dev/patika/notificationservice/model/AbstractBaseEntity.java
@@ -0,0 +1,26 @@
+package dev.patika.notificationservice.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private Instant createdDate= Instant.now();
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant lastModifiedDate=Instant.now();
+}
