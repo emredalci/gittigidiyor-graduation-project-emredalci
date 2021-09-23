19bbce23af1e5a30bdd9fb20d8e53be71f09e766
Emre <emredalc@gmail.com>
2021-09-23 19:55:11 +0300

    Update CustomerUpdateDTO.java -> Add pattern

--- customer-service/src/main/java/dev/patika/customerservice/dto/CustomerUpdateDTO.java
+++ customer-service/src/main/java/dev/patika/customerservice/dto/CustomerUpdateDTO.java
@@ -1,32 +1,40 @@
 package dev.patika.customerservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerUpdateDTO {
 
-    @NotNull
-    private long id;
-
-    @NotBlank
+    @ApiModelProperty(example = "Emre")
+    @NotBlank(message = "First name is mandatory")
+    @Pattern(regexp = "^[\\p{L}]+([(\\s)$]?+[\\p{L}]+)+",message = "Invalid first name")
     private String firstName;
 
-    @NotBlank
+    @ApiModelProperty(example = "Dalcı")
+    @NotBlank(message = "Last name is mandatory")
+    @Pattern(regexp = "^[\\p{L}]+([(\\s)$]?+[\\p{L}]+)+",message = "Invalid last name")
     private String lastName;
+    
 
-    @NotBlank
-    private String nationalId;
+    @ApiModelProperty(example = "1000")
+    @NotNull(message = "Income is mandatory")
+    @Range(max = 100000,min = 0,message = "Invalid income")
+    @JsonProperty(required = true)
+    private Double income;
 
-    @NotNull
-    private double income;
-
-    @NotBlank
+    @ApiModelProperty(example = "05391111111")
+    @NotBlank(message = "Phone number is mandatory")
+    @Pattern(regexp = "^(05)([0-9]{2})\\s?([0-9]{3})\\s?([0-9]{2})\\s?([0-9]{2})$",message = "Invalid phone number")
     private String phoneNumber;
 }
