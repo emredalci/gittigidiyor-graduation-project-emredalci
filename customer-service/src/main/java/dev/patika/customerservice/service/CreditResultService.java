5723e27b0c5ed8c68552aafe9ad68b549f81b7b4
Emre <emredalc@gmail.com>
2021-09-24 23:37:13 +0300

    Add logger

--- customer-service/src/main/java/dev/patika/customerservice/service/CreditResultService.java
+++ customer-service/src/main/java/dev/patika/customerservice/service/CreditResultService.java
@@ -4,6 +4,8 @@ import dev.patika.customerservice.dto.CreditResultResponseDTO;
 import dev.patika.customerservice.mapper.CreditResultMapper;
 import dev.patika.customerservice.repository.CreditResultRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +20,10 @@ public class CreditResultService implements BaseService<CreditResultService>{
     private final CreditResultRepository creditResultRepository;
     private final CreditResultMapper creditResultMapper;
 
+    Logger logger = LoggerFactory.getLogger(CreditResultService.class);
+
     public List<CreditResultResponseDTO> findByNationalId(String nationalId){
+        logger.info("Credit result service finding customer by national id process is started.");
         return creditResultRepository.findAllByCustomerNationalId(nationalId)
                 .stream().map(creditResultMapper::mapFromCreditResulttoCreditResultResponseDTO)
                 .collect(Collectors.toList());
