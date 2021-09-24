15eb2c5f56592f6e18fd7dbe0a7007c14724a00e
Emre <emredalc@gmail.com>
2021-09-24 21:33:23 +0300

    Add test methods for CreditScoreRepository

---
+++ customer-service/src/test/java/dev/patika/customerservice/repository/CreditScoreRepositoryTest.java
@@ -0,0 +1,50 @@
+package dev.patika.customerservice.repository;
+
+import dev.patika.customerservice.model.CreditScore;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class CreditScoreRepositoryTest {
+
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @Autowired
+    CreditScoreRepository creditScoreRepository;
+
+    @InjectMocks
+    CreditScore creditScore;
+
+    @BeforeEach
+    public void setUp(){
+        creditScore = new CreditScore(1L,'4',1000);
+        creditScoreRepository.save(creditScore);
+    }
+
+    @AfterEach
+    public void cleanUp(){
+        creditScore=null;
+    }
+
+
+
+    @Test
+    void findCreditScore() {
+        //given
+        //when
+        double expected=creditScoreRepository.findCreditScore(creditScore.getLastNumber());
+        //then
+        assertNotNull(expected);
+        assertEquals(expected,creditScore.getScore());
+    }
+}
