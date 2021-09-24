34faa0658a70f4e12d683acd7fe7ebe9432a57c2
Emre <emredalc@gmail.com>
2021-09-24 21:31:12 +0300

    Add test methods for CreditResultRepository

---
+++ customer-service/src/test/java/dev/patika/customerservice/repository/CreditResultRepositoryTest.java
@@ -0,0 +1,63 @@
+package dev.patika.customerservice.repository;
+
+
+import dev.patika.customerservice.model.CreditResult;
+import dev.patika.customerservice.model.enumeration.Status;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class CreditResultRepositoryTest {
+
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @Autowired
+    CreditResultRepository creditResultRepository;
+
+    @InjectMocks
+    private CreditResult creditResult;
+
+    @BeforeEach
+    public void setUp(){
+        creditResult = new CreditResult(Status.ACCEPT,5000,"13758028554");
+        creditResult.setId(1L);
+        creditResult.setCreatedDate(Instant.now());
+        creditResult.setLastModifiedDate(Instant.now());
+        creditResultRepository.save(creditResult);
+    }
+
+    @AfterEach
+    public void cleanUp(){
+        creditResult = null;
+    }
+
+
+
+    @Test
+    void findAllByCustomerNationalId() {
+        //given
+        //when
+        List<CreditResult> actaul = creditResultRepository.findAllByCustomerNationalId(creditResult.getCustomerNationalId());
+        //then
+        assertAll(
+                ()-> assertNotNull(actaul),
+                ()-> assertEquals(creditResult.getStatus(),actaul.get(0).getStatus()),
+                ()-> assertEquals(creditResult.getCustomerNationalId(),actaul.get(0).getCustomerNationalId()),
+                ()-> assertEquals(creditResult.getCreditLimit(),actaul.get(0).getCreditLimit())
+        );
+    }
+}
