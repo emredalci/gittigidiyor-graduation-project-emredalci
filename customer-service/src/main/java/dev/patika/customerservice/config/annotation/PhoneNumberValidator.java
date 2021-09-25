b3547b3bb5b5fde33086e4f9cdc1e51fdc8101ae
Emre <emredalc@gmail.com>
2021-09-25 09:45:16 +0300

    Create phone number annotation

---
+++ customer-service/src/main/java/dev/patika/customerservice/config/annotation/PhoneNumberValidator.java
@@ -0,0 +1,22 @@
+package dev.patika.customerservice.config.annotation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumberValidator implements ConstraintValidator<PhoneNumber,String> {
+
+    private static final String PHONE_NUMBER_REGEX = "^(05)([0-9]{2})\\s?([0-9]{3})\\s?([0-9]{2})\\s?([0-9]{2})$";
+    private static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile(PHONE_NUMBER_REGEX);
+    @Override
+    public void initialize(PhoneNumber constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
+        Matcher matcher = PHONE_NUMBER_PATTERN.matcher(phoneNumber);
+        return matcher.matches();
+    }
+}
