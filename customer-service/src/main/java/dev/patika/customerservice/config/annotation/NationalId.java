27ad8e4a05034709502aff289faaa352a7f373c9
Emre <emredalc@gmail.com>
2021-09-25 09:48:02 +0300

    Create national id annotation

---
+++ customer-service/src/main/java/dev/patika/customerservice/config/annotation/NationalId.java
@@ -0,0 +1,15 @@
+package dev.patika.customerservice.config.annotation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = NationalIdValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NationalId {
+    String message() default "Invalid national id";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ customer-service/src/main/java/dev/patika/customerservice/config/annotation/NationalIdValidator.java
@@ -0,0 +1,23 @@
+package dev.patika.customerservice.config.annotation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NationalIdValidator implements ConstraintValidator<NationalId,String> {
+
+    private static final String NATIONAL_ID_REGEX = "^[1-9]{1}[0-9]{9}[02468]{1}$";
+    private static final Pattern NATIONAL_ID_PATTERN = Pattern.compile(NATIONAL_ID_REGEX);
+
+    @Override
+    public void initialize(NationalId constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String nationalId, ConstraintValidatorContext context) {
+        Matcher matcher=NATIONAL_ID_PATTERN.matcher(nationalId);
+        return matcher.matches();
+    }
+}
