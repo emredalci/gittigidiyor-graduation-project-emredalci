029c7127a119ffcbd955fc9ce4256d1fbd5739cb
Emre <emredalc@gmail.com>
2021-09-29 22:06:53 +0300

    Update validations

--- customer-service/src/main/java/dev/patika/customerservice/dto/CustomerLoggerResponseDTO.java
+++ customer-service/src/main/java/dev/patika/customerservice/dto/CustomerLoggerResponseDTO.java
@@ -1,8 +1,14 @@
 package dev.patika.customerservice.dto;
 
+import dev.patika.customerservice.config.annotation.Name;
+import dev.patika.customerservice.config.annotation.NationalId;
+import dev.patika.customerservice.config.annotation.PhoneNumber;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -11,22 +17,38 @@ public class CustomerLoggerResponseDTO {
 
     private Long id;
 
+    @Name
+    @NotBlank
     private String firstNameBefore;
 
+    @Name
+    @NotBlank
     private String firstNameAfter;
 
+    @Name
+    @NotBlank
     private  String lastNameBefore;
 
+    @Name
+    @NotBlank
     private String lastNameAfter;
 
+    @NotNull
     private Double incomeBefore;
 
+    @NotNull
     private Double incomeAfter;
 
+    @PhoneNumber
+    @NotBlank
     private String phoneNumberBefore;
 
+    @PhoneNumber
+    @NotBlank
     private String phoneNumberAfter;
 
+    @NationalId
+    @NotBlank
     private String nationalId;
 
 }
